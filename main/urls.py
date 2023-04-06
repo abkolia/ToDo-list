981c2ea202fccd6d42e71c40c83dd940c1c7c79c
abkolia <vkkolyanabdullaev@gmail.com>
2023-04-06 20:10:56 +0700

    Добавленна аутентификация и выход из аккаунта

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from .views import TasksListView, TaskCreateView, TaskDetailView, TaskUpdateView, TaskDeleteView
+from .views import CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 app_name = 'main'
 
 urlpatterns = [
+    path('login', CustomLoginView.as_view(), name='login_page'),
+    path('logout', LogoutView.as_view(next_page='main:login_page'), name='logout_page'),
     path('', TasksListView.as_view(), name='list_page'),
     path('create', TaskCreateView.as_view(), name='create_page'),
     path('detail_task/<int:pk>', TaskDetailView.as_view(), name='detail_page'),
--- main/views.py
+++ main/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DeleteView, DetailView, UpdateView
 from .models import TodoModel
+from django.contrib.auth.views import LoginView
 
+class CustomLoginView(LoginView):
+    template_name = 'main/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('main:list_page')
 
 class TasksListView(ListView):
     model = TodoModel
