45aadf10c27fd98c65b2cd0e9566bca148cb5fc3
abkolia <vkkolyanabdullaev@gmail.com>
2023-04-12 20:25:21 +0700

    Deleted trash

--- ToDoList/urls.py
+++ ToDoList/urls.py
@@ -2,12 +2,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
-from django.views.static import serve
-from django.conf.urls import url
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path('', include('main.urls'))
 ]
